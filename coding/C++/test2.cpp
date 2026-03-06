ffa27ad7286879a1c62521ae070e137424eb6085
PLPOGBA <kangjinoo@knu.ac.kr>
2026-03-06 10:36:24 +0900

    feat: implement login functionality

---
+++ coding/C++/test2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >> N;
+
+    int A[N][N];
+    int B[N][N];
+
+    for (int i =0; i<N; i++){
+       
+    }
+}
